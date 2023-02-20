299188c5fd6565265024f23f441a5bb93d0f1e90
Jorge Rangel <jorgerangel@192.168.1.3>
2023-02-19 21:31:06 -0600

    Update version number of utils

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -12,23 +12,86 @@ const main = async () => {
     await fs.remove(targetDir)
 
     const packageDir = path.resolve(sourceDir, 'package.json')
-    const packageData = await fs.readJSON(packageDir)
+    await fs.copy(packageDir, path.resolve(targetDir, 'package.json'))
 
-    packageData.type = 'commonjs'
-    const libPackagePath = path.resolve(targetDir, 'package.json')
-    await fs.ensureFile(libPackagePath)
-    await fs.writeJSON(libPackagePath, packageData, { spaces: 4 })
+    const declarationDir = path.resolve(targetDir, 'types')
+
+    await execa('npx', [
+        'tsc',
+        '--declaration',
+        '--emitDeclarationOnly',
+        '--outDir',
+        declarationDir,
+    ], {
+        cwd: sourceDir,
+        stdio: 'inherit',
+    })
+
+    const declarationFiles = await fs.readdir(declarationDir)
+
+    await Promise.all(declarationFiles.map(async (declarationFileName) => {
+        const declarationFilePath = path.resolve(declarationDir, declarationFileName)
+        const declarationFileSource = (await fs.readFile(declarationFilePath)).toString()
+        await fs.writeFile(declarationFilePath, declarationFileSource.replace('export default', 'export ='))
+    }))
+
+    const commonJsDir = path.resolve(targetDir, 'cjs')
 
     await execa('npx', [
         'tsc',
         '--outDir',
-        targetDir,
+        commonJsDir,
         '--module',
         'commonjs',
     ], {
         cwd: sourceDir,
         stdio: 'inherit',
     })
+
+    const commonJsFiles = await fs.readdir(commonJsDir)
+
+    await Promise.all(commonJsFiles.map(async (fileName) => {
+        const filePath = path.resolve(commonJsDir, fileName)
+        const fileSource = (await fs.readFile(filePath)).toString()
+        const newSource = fileSource
+            .replace('\nObject.defineProperty(exports, "__esModule", { value: true });', '')
+            .replace('exports.default = ', 'module.exports = ')
+        await fs.writeFile(filePath, newSource)
+    }))
+
+    const es6Dir = path.resolve(targetDir, 'es6')
+
+    await execa('npx', [
+        'tsc',
+        '--outDir',
+        es6Dir,
+        '--module',
+        'ES6',
+    ], {
+        cwd: sourceDir,
+        stdio: 'inherit',
+    })
+
+    const es6Files = await fs.readdir(es6Dir)
+
+    await Promise.all(es6Files.map(async (fileName) => {
+        if (fileName.endsWith('.d.ts')) {
+            return
+        }
+        const filePath = path.resolve(es6Dir, fileName)
+        let fileContent = (await fs.readFile(filePath)).toString()
+        const matches = fileContent.match(/'\.\/[^]+?'/)
+
+        if (matches) {
+            matches?.forEach((match) => {
+                fileContent = fileContent.replace(match, match.replace(/'$/, ".mjs'"))
+            })
+        }
+
+        await fs.writeFile(filePath, fileContent)
+
+        await fs.rename(filePath, filePath.replace(/\.js$/, '.mjs'))
+    }))
 
     await execa('npm', [
         'publish',
--- src/valueIsDatabaseSchemaColumnDefinition.ts
+++ src/valueIsDatabaseSchemaColumnDefinition.ts
@@ -2,9 +2,10 @@ import buildValueIsConstant from 'ts-validators/buildValueIsConstant'
 import buildValueIsOneOf from 'ts-validators/buildValueIsOneOf'
 import buildValueIsShape from 'ts-validators/buildValueIsShape'
 import valueIsString from 'ts-validators/valueIsString'
+import type { Validator } from 'ts-validators/types'
 import type { ColumnDefinition } from './types'
 
-const valueIsDatabaseSchemaColumnDefinition = buildValueIsShape<ColumnDefinition>('ColumnDefinition', {
+const valueIsDatabaseSchemaColumnDefinition: Validator<ColumnDefinition> = buildValueIsShape<ColumnDefinition>('ColumnDefinition', {
     Field: valueIsString,
     Null: buildValueIsOneOf([
         buildValueIsConstant('NO'),
